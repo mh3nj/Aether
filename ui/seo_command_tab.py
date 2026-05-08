ee6f1e9bd5369a743b7d4eca94936a8f63dfdd19
Mohsen Jafari <anoushzadparsi@gmail.com>
2026-05-08 08:23:49 +0000

    Update seo_command_tab.py

--- ui/seo_command_tab.py
+++ ui/seo_command_tab.py
@@ -33,6 +33,44 @@ class SEOCommandTab(QWidget):
         layout.addWidget(self.tabs)
     
     def update_theme(self, is_dark):
+        if is_dark:
+            self.tabs.setStyleSheet("""
+                QTabWidget::pane {
+                    background-color: #1E1F22;
+                    border: 1px solid #3E4045;
+                }
+                QTabBar::tab {
+                    background-color: #2B2D31;
+                    color: #E8E8E8;
+                    padding: 6px 12px;
+                }
+                QTabBar::tab:selected {
+                    background-color: #3E4045;
+                }
+                QTabBar::tab:hover {
+                    background-color: #4B4E54;
+                }
+            """)
+        else:
+            self.tabs.setStyleSheet("""
+                QTabWidget::pane {
+                    background-color: #FFFFFF;
+                    border: 1px solid #D0D7DE;
+                }
+                QTabBar::tab {
+                    background-color: #F1F3F5;
+                    color: #2C3E50;
+                    padding: 6px 12px;
+                }
+                QTabBar::tab:selected {
+                    background-color: #FFFFFF;
+                }
+                QTabBar::tab:hover {
+                    background-color: #8095AB;
+                    color: white;
+                }
+            """)
+        
         for tab in [self.seo_tab, self.score_tab, self.duplicate_tab, self.meta_refresh_tab]:
             if hasattr(tab, 'update_theme'):
                 tab.update_theme(is_dark)
